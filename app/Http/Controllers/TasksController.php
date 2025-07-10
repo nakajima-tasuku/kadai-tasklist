57bdd36d9f40c5d9070567a28e4cec5e54b8608a
nakajima-tasuku <nakajima-tasuku@agex.co.jp>
2025-07-10 18:26:50 +0900

    validate and add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -37,8 +37,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',
+        ]);
+
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
@@ -73,9 +79,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',
+        ]);
+
         $task = Task::findOrFail($id);
 
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
